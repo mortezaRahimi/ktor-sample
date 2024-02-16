569c4209777cada2b8e7c48b8426118c63963761
Morteza Rahimi <rahimi.morteza53266@gmail.com>
2024-02-16 21:59:58 +0330

    Group chat config added.

--- src/main/kotlin/com/example/plugins/SocketRouting.kt
+++ src/main/kotlin/com/example/plugins/SocketRouting.kt
@@ -10,6 +10,8 @@ import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
 import java.time.Duration
+import java.util.*
+import kotlin.collections.LinkedHashSet
 
 fun Application.configureSockets() {
     install(WebSockets) {
@@ -19,12 +21,39 @@ fun Application.configureSockets() {
         masking = false
     }
     routing {
+
+        //one client
+//        webSocket("/chat") {
+//            send("You are connected!")
+//            for(frame in incoming) {
+//                frame as? Frame.Text ?: continue
+//                val receivedText = frame.readText()
+//                send("You said: $receivedText")
+//            }
+//        }
+
+        //chat room
+
+        val connections = Collections.synchronizedSet<Connection?>(LinkedHashSet())
         webSocket("/chat") {
-            send("You are connected!")
-            for(frame in incoming) {
-                frame as? Frame.Text ?: continue
-                val receivedText = frame.readText()
-                send("You said: $receivedText")
+            println("Adding user!")
+            val thisConnection = Connection(this)
+            connections += thisConnection
+            try {
+                send("You are connected! There are ${connections.count()} users here.")
+                for (frame in incoming) {
+                    frame as? Frame.Text ?: continue
+                    val receivedText = frame.readText()
+                    val textWithUsername = "[${thisConnection.name}]: $receivedText"
+                    connections.forEach {
+                        it.session.send(textWithUsername)
+                    }
+                }
+            } catch (e: Exception) {
+                println(e.localizedMessage)
+            } finally {
+                println("Removing $thisConnection!")
+                connections -= thisConnection
             }
         }
     }
